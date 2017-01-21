84faee719690a695e90edf3111a0ba0b1a8ab714
Matthew <gnemlock@gmail.com>
2017-01-22 00:25:39 +1100

    Initial Upload
    Initial Upload

---
+++ Assets/Script/Path.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Debugging;
+
+
+/// <summary></summary>
+/// TODO: Convert to standalone script for implementation in core character class.
+public class Path : MonoBehaviour 
+{
+		 public Vector3[] waypoint;
+		 public float[] distance;
+		 public float totalDistance;
+
+		 public virtual void Awake()
+		 {
+					distance = new float[(waypoint.Length - 2)];
+					SetDistances ();
+		 }
+
+		 protected virtual void SetDistances()
+		 {
+					int currentIndex = 0;
+					int endIndex = (waypoint.Length - 1);
+					totalDistance = 0f;
+
+					while (currentIndex < endIndex)
+					{
+							 distance[currentIndex] = Vector3.Distance (waypoint[currentIndex], waypoint[currentIndex + 1]);
+							 totalDistance += distance [currentIndex++];
+					}
+		 }
+}
+
+public class Circuit : Path
+{
+
+		 public override void Awake()
+		 {
+					distance = new float[(waypoint.Length - 1)];
+					SetDistances ();
+		 }
+
+		 protected override void SetDistances()
+		 {
+					int currentIndex = 0;
+					int endIndex = (waypoint.Length - 1);
+					totalDistance = 0f;
+
+					while (currentIndex < endIndex)
+					{
+							 distance [currentIndex] = Vector3.Distance (waypoint[currentIndex], waypoint[currentIndex + 1]);
+							 totalDistance += distance [currentIndex++];
+					}
+
+					distance [endIndex] = Vector3.Distance (waypoint [endIndex], waypoint [0]);
+					totalDistance += distance [endIndex];
+		 }
+}
---
+++ Assets/Script/SecurityCamera.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+
+//TODO:The SecurityCamera should allow for multiple movement variations
+//TODO:Implement still camera variation
+//TODO:Implement basic point to point panning
+//TODO:Test trigger events
+//TODO:Enable ability to track player
+//TODO:Ensure camera can return to previous activity when loosing tracked player
+//TODO:Set up constructors
+//TODO:Set up deconstructors
+//TODO:Set up room reference
+//TODO:Set up source reference
+//TODO:Set up hijack ability
+//TODO:Set up main control for quick access
+
+public class SecurityCamera : MonoBehaviour 
+{
+		 /// <summary>Keeps static reference of the next available id to ensure unique id values.</summary>
+		 private static int nextId = 0;
+		 /// <summary>The id reference for this <see cref="SecurityCamera"/>.</summary>
+		 public int id { get; protected set;}
+		 /// <summary>Indicates wether this <see cref="SecurityCamera"/> is currently feeding 
+		 /// information back to its source.</summary>
+		 public bool feeding { get; protected set;}
+		 /// <summary>Indicates wether this <see cref="SecurityCamera"/> is currently turned on.</summary>
+		 public bool active { get; protected set;}
+		 /// <summary>The pivot point used for rotation.</summary>
+		 [SerializeField] private Transform pivotPoint;
+		 [SerializeField] private Transform lens;
+
+		 /// <summary>Delegate for events that fire when the camera detects activity (or inactivity).</summary>
+		 private delegate void DetectActivityEvent(int cameraId);
+		 /// <summary>Occurs when the camera detects any movement and is 
+		 /// both <see cref="active"/> and <see cref="feeding"/>.</summary>
+		 /// <remarks>This event should fire whenever the kinematic collider representing this 
+		 /// <see cref="SecurityCamera"/>s field of vision is triggered.</remarks>
+		 public static event DetectActivityEvent DetectActivity;
+		 /// <summary>Occurs when the camera detects movement by units hostile to the player and is 
+		 /// both <see cref="active"/> and <see cref="feeding"/>.</summary>
+		 /// <remarks>This event should fire whenever a <see cref="DetectActivity"/> event fires from a 
+		 /// <see cref="GameObject"/> tagged as "Enemy".</remarks>
+		 public static event DetectActivityEvent DetectEnemyActivity;
+		 /// <summary>Occurs when the camera detects movement by units friendly to the player and is 
+		 /// both <see cref="active"/> and <see cref="feeding"/>.</summary>
+		 /// <remarks>This event should fire whenever a <see cref="DetectActivity"/> event fires from a 
+		 /// <see cref="GameObject"/> that is not tagged as "Player" or "Enemy".</remarks>
+		 public static event DetectActivityEvent DetectFriendlyActivity;
+		 /// <summary>Occurs when the camera detects movement by the player and is 
+		 /// both <see cref="active"/> and <see cref="feeding"/>.</summary>
+		 /// <remarks>This event should fire whenever a <see cref="DetectActivity"/> event fires from a 
+		 /// <see cref="GameObject"/> tagged as "Player".</remarks>
+		 public static event DetectActivityEvent DetectPlayer;
+		 /// <summary>Occurs when the camera loses detection of the player and is 
+		 /// both <see cref="active"/> and <see cref="feeding"/>.</summary>
+		 /// <remarks>This event should fire whenever the kinematic collider representing 
+		 /// this <see cref="SecurityCamera"/>s field of vision detects losing a 
+		 /// <see cref="GameObject"/> tagged as "Player".</remarks>
+		 public static event DetectActivityEvent LosePlayer;
+
+		 void Awake()
+		 {
+					// Allocate the next available id to this instance, and increment nextID.
+					id = nextId++;
+
+		 #if UNITY_EDITOR
+					// If we are in the Unity Editor, ensure that our pivot point is linked up.
+					if(pivotPoint == null)
+					{
+							 // If it isn't, output a warning to the debug log.
+							 Debug.Log("Warning: Pivot point not set up");
+					}
+		 #endif
+
+		 }
+
+		 void Update()
+		 {
+					if (active)
+					{
+					}
+		 }
+
+		 void OnTriggerEnter(Collider otherCollider)
+		 {
+					if (active && feeding)
+					{
+							 if (DetectActivity != null)
+							 {
+										DetectActivity (id);
+							 }
+
+							 if (otherCollider.CompareTag ("Player"))
+							 {
+										if (DetectPlayer != null)
+										{
+												 DetectPlayer (id);
+										}
+							 } 
+							 else if (otherCollider.CompareTag ("Enemy"))
+							 {
+										if (DetectEnemyActivity != null)
+										{
+												 DetectEnemyActivity (id);
+										}
+							 } 
+							 else
+							 {
+										if (DetectFriendlyActivity != null)
+										{
+												 DetectFriendlyActivity (id);
+										}
+							 }
+					}
+		 }
+
+		 void OnTriggerExit(Collider otherCollider)
+		 {
+					if (active && feeding)
+					{
+							 if (otherCollider.CompareTag ("Player") && LosePlayer != null)
+							 {
+										LosePlayer (id);
+							 }
+					}
+		 }
+}
