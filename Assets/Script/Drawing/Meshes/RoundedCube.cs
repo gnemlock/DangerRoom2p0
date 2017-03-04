6f0e2005eb1f6afc25488344ae4e16574fd9d4b0
Matthew <gnemlock@gmail.com>
2017-03-04 16:53:16 +1100

    Basic Shapes and Placeholders
    Created basic shape meshe RoundedCube and added a few placeholder
    textures

---
+++ Assets/Script/Drawing/Meshes/RoundedCube.cs
@@ -0,0 +1,243 @@
+﻿/* 
+ * Created by Matthew F Keating with the help of Catlike Coding Tutorials.
+ *  --- http://catlikecoding.com/unity/tutorials/rounded-cube/ ---
+ */
+
+using UnityEngine;
+
+namespace Drawing.Meshes
+{
+    [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+    public class RoundedCube : MonoBehaviour
+    {
+        public int width, height, depth;
+        
+        private Mesh mesh;
+        public Vector3[] vertices;
+        
+        private void Awake()
+        {
+            Generate();
+        }
+        
+        #if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            if(vertices != null)
+            {
+                Gizmos.color = Color.black;
+                
+                for(int i = 0; i < vertices.Length; i++)
+                {
+                    Gizmos.DrawSphere(transform.TransformPoint(vertices[i]), 0.1f);
+                }
+            }
+        }
+        #endif
+
+        private void Generate()
+        {
+            GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+            mesh.name = "Procedural Cube";
+            
+            GenerateVertices();
+            GenerateTriangles();
+        }
+        
+        private int GenerateBottomFace(int[] triangles, int triangleIndex, int ringLength)
+        {
+            int vertexIndex = 1;
+            int vertexMinimum = ringLength - 2;
+            int vertexMiddle = vertices.Length - (width - 1) * (depth - 1);
+            int vertexMaximum;
+            int vertexTop;
+            
+            triangleIndex = GenerateQuadTriangles(triangles, triangleIndex, ringLength - 1, 
+                vertexMiddle, 0, 1);
+
+            for(int x = 1; x < width - 1; x++, vertexIndex++, vertexMiddle++)
+            {
+                triangleIndex = GenerateQuadTriangles(triangles, triangleIndex, vertexMiddle, 
+                    vertexMiddle + 1, vertexIndex, vertexIndex + 1);
+            }
+
+            triangleIndex = GenerateQuadTriangles(triangles, triangleIndex, vertexMiddle, 
+                vertexIndex + 2, vertexIndex, vertexIndex + 1);
+
+            vertexMiddle -= width - 2;
+            vertexMaximum = vertexIndex + 2;
+
+            for(int z = 1; z < depth - 1; z++, vertexMinimum--, vertexMiddle++, vertexMaximum++)
+            {
+                triangleIndex = GenerateQuadTriangles(triangles, triangleIndex, vertexMinimum, 
+                    vertexMiddle + width - 1, vertexMinimum + 1, vertexMiddle);
+
+                for(int x = 1; x < width - 1; x++, vertexMiddle++)
+                {
+                    triangleIndex = GenerateQuadTriangles(triangles, triangleIndex, 
+                        vertexMiddle + width - 1, vertexMiddle + width, vertexMiddle, 
+                        vertexMiddle + 1);
+                }
+
+                triangleIndex = GenerateQuadTriangles(triangles, triangleIndex, 
+                    vertexMiddle + width - 1, vertexMaximum + 1, vertexMiddle, vertexMaximum);
+            }
+
+            vertexTop = vertexMinimum - 1;
+
+            triangleIndex = GenerateQuadTriangles(triangles, triangleIndex, vertexTop + 1, 
+                vertexTop, vertexTop + 2, vertexMiddle);
+
+            for(int x = 1; x < width - 1; x++, vertexTop--, vertexMiddle++)
+            {
+                triangleIndex = GenerateQuadTriangles(triangles, triangleIndex, vertexTop, 
+                    vertexTop - 1, vertexMiddle, vertexMiddle + 1);
+            }
+
+            triangleIndex = GenerateQuadTriangles(triangles, triangleIndex, vertexTop, 
+                vertexTop - 1, vertexMiddle, vertexTop - 2);
+
+            return triangleIndex;
+        }
+        
+        private int GenerateTopFace(int[] triangles, int triangleIndex, int ringLength)
+        {
+            int vertexIndex = ringLength * height;
+            int vertexMinimum = ringLength * (height + 1) - 1;
+            int vertexMiddle = vertexMinimum + 1;
+            int vertexMaximum;
+            int vertexTop;
+
+            for(int x = 0; x < width - 1; x++, vertexIndex++)
+            {
+                triangleIndex = GenerateQuadTriangles(triangles, triangleIndex, vertexIndex, 
+                    vertexIndex + 1, vertexIndex + ringLength - 1, vertexIndex + ringLength);
+            }
+            
+            vertexMaximum = vertexIndex + 2;
+
+            triangleIndex = GenerateQuadTriangles(triangles, triangleIndex, vertexIndex, 
+                vertexIndex + 1, vertexIndex + ringLength - 1, vertexIndex + 2);
+            
+            for(int z = 1; z < depth - 1; z++, vertexMinimum--, vertexMiddle++, vertexMaximum++)
+            {
+                triangleIndex = GenerateQuadTriangles(triangles, triangleIndex, vertexMinimum, 
+                    vertexMiddle, vertexMinimum - 1, vertexMiddle + width - 1);
+            
+                for(int x = 1; x < width - 1; x++, vertexMiddle++)
+                {
+                    triangleIndex = GenerateQuadTriangles(triangles, triangleIndex, vertexMiddle, 
+                        vertexMiddle + 1, vertexMiddle + width - 1, vertexMiddle + width);
+                }
+            
+                triangleIndex = GenerateQuadTriangles(triangles, triangleIndex, vertexMiddle, 
+                    vertexMaximum, vertexMiddle + width - 1, vertexMaximum + 1);
+            }
+            
+           vertexTop = vertexMinimum - 2;
+            
+            triangleIndex = GenerateQuadTriangles(triangles, triangleIndex, vertexMinimum, 
+                vertexMiddle, vertexMinimum - 1, vertexMinimum - 2);
+            
+            for(int x = 1; x < width - 1; x++, vertexTop--, vertexMiddle++)
+            {
+                triangleIndex = GenerateQuadTriangles(triangles, triangleIndex, vertexMiddle, 
+                    vertexMiddle + 1, vertexTop, vertexTop - 1);
+            }
+            
+            triangleIndex = GenerateQuadTriangles(triangles, triangleIndex, vertexMiddle, 
+                vertexTop - 2, vertexTop, vertexTop - 1);
+
+            return triangleIndex;
+        }
+        
+        private void GenerateTriangles()
+        {
+            int quadCount = (width * height + width * depth + height * depth) * 2;
+            int ringLength = (width + depth) * 2;
+            int[] triangles = new int[quadCount * 6];
+            int triangleIndex = 0, vertexIndex = 0;
+            
+            for(int y = 0; y < height; y++, vertexIndex++)
+            {
+            
+                for(int quadIndex = 0; quadIndex < ringLength - 1; quadIndex++, vertexIndex++)
+                {
+                    triangleIndex = GenerateQuadTriangles(triangles, triangleIndex, vertexIndex, 
+                        vertexIndex + 1, vertexIndex + ringLength, vertexIndex + ringLength + 1);
+                }
+            
+                triangleIndex = GenerateQuadTriangles(triangles, triangleIndex, vertexIndex, 
+                    vertexIndex - ringLength + 1, vertexIndex + ringLength, vertexIndex + 1);
+            }
+            
+            triangleIndex = GenerateTopFace(triangles, triangleIndex, ringLength);
+            triangleIndex = GenerateBottomFace(triangles, triangleIndex, ringLength);
+            
+            mesh.triangles = triangles;
+        }
+        
+        private void GenerateVertices()
+        {
+            int cornerVerticesCount = 8;
+            int edgeVerticesCount = (width + depth + height - 3) * 4;
+            int faceVerticesCount = ((width - 1) * (height - 1) + (width - 1) * (depth - 1)
+                + (height - 1) * (depth - 1)) * 2;
+            int vertexIndex = 0;
+
+            vertices = new Vector3[cornerVerticesCount + edgeVerticesCount + faceVerticesCount];
+
+            for(int y = 0; y <= height; y++)
+            {
+                for(int x = 0; x <= width; x++)
+                {
+                    vertices[vertexIndex++] = new Vector3(x, y, 0);
+                }
+
+                for(int z = 1; z <= depth; z++)
+                {
+                    vertices[vertexIndex++] = new Vector3(width, y, z);
+                }
+
+                for(int x = width - 1; x >= 0; x--)
+                {
+                    vertices[vertexIndex++] = new Vector3(x, y, depth);
+                }
+
+                for(int z = depth - 1; z > 0; z--)
+                {
+                    vertices[vertexIndex++] = new Vector3(0, y, z);
+                }
+            }
+
+            for(int z = 1; z < depth; z++)
+            {
+                for(int x = 1; x < width; x++)
+                {
+                    vertices[vertexIndex++] = new Vector3(x, height, z);
+                }
+            }
+
+            for(int z = 1; z < depth; z++)
+            {
+                for(int x = 1; x < width; x++)
+                {
+                    vertices[vertexIndex++] = new Vector3(x, 0, z);
+                }
+            }
+
+            mesh.vertices = vertices;
+        }
+        
+        private static int GenerateQuadTriangles(int[] triangles, int index, int bottomLeftIndex, 
+            int bottomRightIndex, int topLeftIndex, int topRightIndex)
+        {
+            triangles[index] = bottomLeftIndex;
+            triangles[index + 1] = triangles[index + 4] = topLeftIndex;
+            triangles[index + 2] = triangles[index + 3] = bottomRightIndex;
+            triangles[index + 5] = topRightIndex;
+            
+            return index + 6;
+        }
+    }
+}
---
+++ Assets/Tutorial Files/Cube.cs
@@ -0,0 +1,164 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class Cube : MonoBehaviour {
+
+    	public int width, height, depth;
+
+	private Mesh mesh;
+	private Vector3[] vertices;
+
+    private void Awake () {
+		Generate();
+	}
+
+	private void Generate () {
+		GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+		mesh.name = "Procedural Cube";
+        CreateVertices();
+		CreateTriangles();
+	}
+
+	private void CreateVertices () {
+		int cornerVertices = 8;
+		int edgeVertices = (width + height + depth - 3) * 4;
+		int faceVertices = (
+			(width - 1) * (height - 1) +
+			(width - 1) * (depth - 1) +
+			(height - 1) * (depth - 1)) * 2;
+		vertices = new Vector3[cornerVertices + edgeVertices + faceVertices];
+
+		int v = 0;
+		for (int y = 0; y <= height; y++) {
+			for (int x = 0; x <= width; x++) {
+				vertices[v++] = new Vector3(x, y, 0);
+			}
+			for (int z = 1; z <= depth; z++) {
+				vertices[v++] = new Vector3(width, y, z);
+			}
+			for (int x = width - 1; x >= 0; x--) {
+				vertices[v++] = new Vector3(x, y, depth);
+			}
+			for (int z = depth - 1; z > 0; z--) {
+				vertices[v++] = new Vector3(0, y, z);
+			}
+		}
+		for (int z = 1; z < depth; z++) {
+			for (int x = 1; x < width; x++) {
+				vertices[v++] = new Vector3(x, height, z);
+			}
+		}
+		for (int z = 1; z < depth; z++) {
+			for (int x = 1; x < width; x++) {
+				vertices[v++] = new Vector3(x, 0, z);
+			}
+		}
+
+		mesh.vertices = vertices;
+	}
+
+	private void CreateTriangles ()
+    {
+        int quadCount = (width * height + width * depth + height * depth) * 2;
+		int[] triangles = new int[quadCount * 6];
+        int ringSize = (width + depth) * 2;
+        int triangleIndex = 0, vertexIndex = 0;
+
+		for (int y = 0; y < height; y++, vertexIndex++) {
+        for (int quadIndex = 0; quadIndex < ringSize - 1; quadIndex++, vertexIndex++) {
+				triangleIndex = SetQuad(triangles, triangleIndex, vertexIndex, vertexIndex + 1, vertexIndex + ringSize, vertexIndex + ringSize + 1);
+			}
+			triangleIndex = SetQuad(triangles, triangleIndex, vertexIndex, vertexIndex - ringSize + 1, vertexIndex + ringSize, vertexIndex + 1);
+		}
+
+		triangleIndex = CreateTopFace(triangles, triangleIndex, ringSize);
+		triangleIndex = CreateBottomFace(triangles, triangleIndex, ringSize);
+		mesh.triangles = triangles;
+	}
+
+	private int CreateTopFace (int[] triangles, int t, int ring) {
+		int v = ring * height;
+		for (int x = 0; x < width - 1; x++, v++) {
+			t = SetQuad(triangles, t, v, v + 1, v + ring - 1, v + ring);
+		}
+		t = SetQuad(triangles, t, v, v + 1, v + ring - 1, v + 2);
+
+		int vMin = ring * (height + 1) - 1;
+		int vMid = vMin + 1;
+		int vMax = v + 2;
+
+		for (int z = 1; z < depth - 1; z++, vMin--, vMid++, vMax++) {
+			t = SetQuad(triangles, t, vMin, vMid, vMin - 1, vMid + width - 1);
+			for (int x = 1; x < width - 1; x++, vMid++) {
+				t = SetQuad(
+					triangles, t,
+					vMid, vMid + 1, vMid + width - 1, vMid + width);
+			}
+			t = SetQuad(triangles, t, vMid, vMax, vMid + width - 1, vMax + 1);
+		}
+
+		int vTop = vMin - 2;
+		t = SetQuad(triangles, t, vMin, vMid, vTop + 1, vTop);
+		for (int x = 1; x < width - 1; x++, vTop--, vMid++) {
+			t = SetQuad(triangles, t, vMid, vMid + 1, vTop, vTop - 1);
+		}
+		t = SetQuad(triangles, t, vMid, vTop - 2, vTop, vTop - 1);
+
+		return t;
+	}
+ 
+	private int CreateBottomFace (int[] triangles, int t, int ring) {
+		int v = 1;
+        int vMid = vertices.Length - (width - 1) * (depth - 1);
+        int test = t;
+		t = SetQuad(triangles, t, ring - 1, vMid, 0, 1);
+
+		for (int x = 1; x < width - 1; x++, v++, vMid++) {
+			t = SetQuad(triangles, t, vMid, vMid + 1, v, v + 1);
+		}
+		t = SetQuad(triangles, t, vMid, v + 2, v, v + 1);
+
+		int vMin = ring - 2;
+		vMid -= width - 2;
+		int vMax = v + 2;
+
+		for (int z = 1; z < depth - 1; z++, vMin--, vMid++, vMax++) {
+			t = SetQuad(triangles, t, vMin, vMid + width - 1, vMin + 1, vMid);
+			for (int x = 1; x < width - 1; x++, vMid++) {
+				t = SetQuad(
+					triangles, t,
+					vMid + width - 1, vMid + width, vMid, vMid + 1);
+			}
+			t = SetQuad(triangles, t, vMid + width - 1, vMax + 1, vMid, vMax);
+		}
+
+		int vTop = vMin - 1;
+		t = SetQuad(triangles, t, vTop + 1, vTop, vTop + 2, vMid);
+		for (int x = 1; x < width - 1; x++, vTop--, vMid++) {
+			t = SetQuad(triangles, t, vTop, vTop - 1, vMid, vMid + 1);
+		}
+		t = SetQuad(triangles, t, vTop, vTop - 1, vMid, vTop - 2);
+
+		return t;
+	}
+
+	private static int
+	SetQuad (int[] triangles, int index, int bottomLeftIndex, int bottomRightIndex, int topLeftIndex, int topRightIndex) 
+    {
+		triangles[index] = bottomLeftIndex;
+		triangles[index + 1] = triangles[index + 4] = topLeftIndex;
+		triangles[index + 2] = triangles[index + 3] = bottomRightIndex;
+        triangles[index + 5] = topRightIndex;
+		return index + 6;
+	}
+
+	private void OnDrawGizmos () {
+		if (vertices == null) {
+			return;
+		}
+		Gizmos.color = Color.black;
+		for (int i = 0; i < vertices.Length; i++) {
+			Gizmos.DrawSphere(vertices[i], 0.1f);
+		}
+	}
+}
