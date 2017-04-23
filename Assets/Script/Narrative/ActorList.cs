b982913eb6a289f9b70bcebd0e0e1c4b8f6f17cf
Matthew <gnemlock@gmail.com>
2017-04-24 00:01:33 +1000

    NarrativeActorListEditor > ActorList
    Implemented game asset interface to move NarrativeActorListEditor data
    to hierarchy as a singleton script. Further functionality
    implementations; moved some strings to utility classes as constants;
    half completed code documentation

--- Assets/Script/Narrative/ActorList.cs
+++ Assets/Script/Narrative/ActorList.cs
@@ -1,11 +1,80 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace Narrative
 {
-    public static class ActorList : MonoBehaviour
+    public class ActorList : MonoBehaviour
     {
-        public static ActorList[] actors;
+        public static ActorList hive;
+        public ActorListing[] actors;
+        public int length
+        { 
+            get 
+            { 
+                try
+                {
+                    return actors.Length;
+                }
+                catch(NullReferenceException exception)
+                {
+                    return 0;
+                }
+            } 
+        }
+        
+        private void Awake()
+        {
+            if(hive == null)
+            {
+                hive = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else if(hive != this)
+            {
+                #if UNITY_EDITOR
+                DestroyImmediate(this);
+                #else
+                Destroy(this);
+                #endif
+            }
+        }
+        
+        public ActorListing this[int index]
+        {
+            get 
+            { 
+                return actors[index]; 
+            }
+            set 
+            { 
+                actors[index] = value; 
+            }
+        }
+    }
+    
+    
+
+    [System.Serializable]
+    public class ActorListing
+    {
+        public string name;
+        public Color colour;
+        public Sprite backingImage;
+        public Font font;
+
+        #if UNITY_EDITOR
+        public void OnGUI()
+        {
+            name = EditorGUILayout.TextField("Name: ", name);
+            colour = EditorGUILayout.ColorField("Text Colour: ", colour);
+            font = (Font)EditorGUILayout.ObjectField("Text Font: ", font, typeof(Font), false);
+            backingImage = (Sprite)EditorGUILayout.ObjectField("Backing Image: ", backingImage, 
+                typeof(Sprite), false);
+        }
+        #endif
     }
 }
