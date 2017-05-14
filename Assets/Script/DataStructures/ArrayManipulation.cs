0a84448163221335eda55e04150e2a85866215a0
Matthew <gnemlock@gmail.com>
2017-05-15 02:30:29 +1000

    VelocityGrapher and ArrayManipulation
    Started on VelocityGrapher to visually graph changes to the velocity of
    a rigidbody; Started on ArrayManipulation to provide static
    functionality in regards to manipulating arrays.

---
+++ Assets/Script/DataStructures/ArrayManipulation.cs
@@ -0,0 +1,135 @@
+﻿using UnityEngine;
+
+namespace DataStructures
+{
+    /// <summary>Static class containing functionality for <see cref="System.Array"/> 
+    /// manipulation.</summary>
+    public static class ArrayManipulation
+    {
+        /// <summary>Resizes an <see cref="System.Array"/> while retaining the data at the end of 
+        /// the array.</summary>
+        /// <param name="array">The <see cref="System.Array"/> to be resized.</param>
+        /// <param name="newSize">The new size of the <see cref="System.Array"/>.</param>
+        /// <param name="defaultValue">The default value to use for nullifying elements in the 
+        /// <see cref="System.Array"/>, where data is moved forward.</param>
+        /// <typeparam name="T">The base type of the <see cref="System.Array"/>.</typeparam>
+        /// <remarks>This method takes an array, and resizes it with 
+        /// <see cref="System.Array.Resize"/>, while attempting to retain data with a "forward" 
+        /// focus. If the <see cref="System.Array"/> is to be shortened, data at the end of the 
+        /// array will be retained. If the <see cref="System.Array"/> is extended, existing data 
+        /// will be pushed to the end. If the <see cref="System.Array"/> is extended, trailing 
+        /// data will be replaced with defaultValue. The <see cref="System.Array"/> will be 
+        /// initialised, if it has not been already initialised.</remarks>
+        public static void ResizeForwardArray<T>(T[] array, int newSize, T defaultValue)
+        {
+            if(array == null)
+            {
+                // If the array has not been initialised, simply initialise it to the newSize.
+                array = new T[newSize];
+            }
+            else
+            {
+                // Else, the array has been initialised; cache the array length, and determine 
+                // the sizeDifference between the newSize and the oldSize.
+                int oldSize = array.Length;
+                int sizeDifference = newSize - oldSize;
+
+                if(sizeDifference > 0)
+                {
+                    // If the difference between the two arrays is greater than 0, we are extending 
+                    // the array; resize it, first, to make room for the new values.
+                    System.Array.Resize(ref array, newSize);
+
+                    for(int i = 1; i < oldSize; i++)
+                    {
+                        // for each element in the original  array, move it forward to the new 
+                        // extended array position, and set the defaultValue to the original 
+                        // position.
+                        array[newSize - i] = array[oldSize - i];
+                        array[oldSize - i] = defaultValue;
+                    }
+                }
+                else if(sizeDifference < 0)
+                {
+                    // Else, If the difference between the two arrays is less than 0, we are 
+                    // shortening the array; we need to move the data, first. Invert the 
+                    // sizeDifference, so we can treat it as a real length.
+                    sizeDifference *= -1;
+
+                    for(int i = oldSize - 1; i > sizeDifference; i--)
+                    {
+                        // for each element in the original array, leading back from the end of the 
+                        // original array to the length of the shortened array, move the 
+                        // corresponding value back, to sit in line with the shortened array.
+                        array[i - sizeDifference] = array[i];
+                    }
+
+                    // Resize the array.
+                    System.Array.Resize(ref array, newSize);
+                }
+            }
+        }
+        
+        /// <summary>Resizes an <see cref="System.Array"/> while retaining the data at the end of 
+        /// the array.</summary>
+        /// <param name="array">The <see cref="System.Array"/> to be resized.</param>
+        /// <param name="newSize">The new size of the <see cref="System.Array"/>.</param>
+        /// <typeparam name="T">The base type of the <see cref="System.Array"/>.</typeparam>
+        /// <remarks>This method takes an array, and resizes it with 
+        /// <see cref="System.Array.Resize"/>, while attempting to retain data with a "forward" 
+        /// focus. If the <see cref="System.Array"/> is to be shortened, data at the end of the 
+        /// array will be retained. If the <see cref="System.Array"/> is extended, existing data 
+        /// will be pushed to the end. If the <see cref="System.Array"/> is extended, trailing 
+        /// data will be replaced with the types default value. The <see cref="System.Array"/> 
+        /// will be initialised, if it has not been already initialised.</remarks>
+        public static void ResizeForwardArray<T>(T[] array, int newSize)
+        {
+            if(array == null)
+            {
+                // If the array has not been initialised, simply initialise it to the newSize.
+                array = new T[newSize];
+            }
+            else
+            {
+                // Else, the array has been initialised; cache the array length, and determine 
+                // the sizeDifference between the newSize and the oldSize.
+                int oldSize = array.Length;
+                int sizeDifference = newSize - oldSize;
+
+                if(sizeDifference > 0)
+                {
+                    // If the difference between the two arrays is greater than 0, we are extending 
+                    // the array; resize it, first, to make room for the new values.
+                    System.Array.Resize(ref array, newSize);
+
+                    for(int i = 1; i < oldSize; i++)
+                    {
+                        // for each element in the original  array, move it forward to the new 
+                        // extended array position, and set the default value of the corresponding 
+                        // type to the original position.
+                        array[newSize - i] = array[oldSize - i];
+                        array[oldSize - i] = default(T);
+                    }
+                }
+                else if(sizeDifference < 0)
+                {
+                    // Else, If the difference between the two arrays is less than 0, we are 
+                    // shortening the array; we need to move the data, first. Invert the 
+                    // sizeDifference, so we can treat it as a real length.
+                    sizeDifference *= -1;
+
+                    for(int i = oldSize - 1; i > sizeDifference; i--)
+                    {
+                        // for each element in the original array, leading back from the end of the 
+                        // original array to the length of the shortened array, move the 
+                        // corresponding value back, to sit in line with the shortened array.
+                        array[i - sizeDifference] = array[i];
+                    }
+
+                    // Resize the array.
+                    System.Array.Resize(ref array, newSize);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Script/Debugging/Physics/VelocityGrapher.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Debugging.Physics
+{
+    
+    public class VelocityGrapher : MonoBehaviour
+    {
+        public int sampleSize;
+        float sampleLength;
+        float maximumVelocity;
+        float minimumVelocity;
+        public int[] velocitySamples;
+        public int newSampleSize;
+        
+        public void AdjustSampleRate(int newSampleSize)
+        {
+            sampleLength = Time.fixedDeltaTime * newSampleSize;
+            
+            if(velocitySamples == null)
+            {
+                velocitySamples = new int[newSampleSize];
+            }
+            else
+            {
+                int sampleSizeDelta = newSampleSize - sampleSize;
+                
+                if(sampleSizeDelta > 0)
+                {
+                    System.Array.Resize(ref velocitySamples, newSampleSize);
+                    
+                    for(int i = 1; i < sampleSizeDelta; i++)
+                    {
+                        velocitySamples[newSampleSize - i] = velocitySamples[sampleSize - i];
+                        velocitySamples[sampleSize - i] = 0;
+                    }
+                }
+                else if(sampleSizeDelta < 0)
+                {
+                    sampleSizeDelta *= -1;
+                    
+                    Debug.Log(newSampleSize);
+                    for(int i = velocitySamples.Length - 1; i > newSampleSize; i--)
+                    {
+                        Debug.Log(velocitySamples.Length + " " + newSampleSize);
+                        Debug.Log(velocitySamples[i - sampleSizeDelta] + " < " + velocitySamples[i]);
+                        velocitySamples[i - sampleSizeDelta] = velocitySamples[i];
+                    }   
+                    System.Array.Resize(ref velocitySamples, newSampleSize);
+                }
+            }
+            
+            //this.sampleSize = sampleSize;
+        }
+        
+        public void AdjustSampleRate(float sampleLength)
+        {
+            int sampleSize = (int)(sampleLength / Time.fixedDeltaTime);
+            AdjustSampleRate(sampleSize);
+        }
+    }
+}
+
+namespace Debugging.Physics.Utility
+{
+    [CustomEditor(typeof(VelocityGrapher))] public class VelocityGrapherEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+            
+            if(GUILayout.Button("AdjustSampleRate"))
+            {
+                VelocityGrapher velocityGrapher = target as VelocityGrapher;
+                
+                velocityGrapher.AdjustSampleRate(velocityGrapher.newSampleSize);
+            }
+        }
+    }
+}
